6d1652aacf260249b8ee894d235e099c22d931af
manvantar <manukvshetty@gmail.com>
2021-03-22 23:07:47 +0530

    calculating wage of employee for 20 days

--- EmpWage.java
+++ EmpWage.java
@@ -6,28 +6,28 @@ public static void main(String[] args){
 	int IS_part_time=2;
 	int wage_per_hour=20;
 	int hours=0;
-	int wage_per_day=0;
-Random rand=new Random();
+	int working_days=20;
+	Random rand=new Random();
 	int empcheck=rand.nextInt(10)%3;
-System.out.println(empcheck+"\n");
 	switch (empcheck){
 		case 1:
 		System.out.println("Employee is Full-timer");
 		hours=12;
-		wage_per_day=hours*wage_per_hour;
-		System.out.println(" wage_per_day ="+ wage_per_day);
 		break;
 		case 2:
 		System.out.println("Employee is Part-timer");
         	hours=8;
-	        wage_per_day=hours*wage_per_hour;
-	        System.out.println(" wage_per_day ="+ wage_per_day);
 		break;
 	
 		default:
 		System.out.println("Employee is Absent");
 
 	}
+
+		int wage_per_day=hours*wage_per_hour;
+                System.out.println(" wage_per_day ="+ wage_per_day);
+		int wage_per_month= working_days*wage_per_day;
+		System.out.println(" wage_per_month ="+ wage_per_month);
 }
 
 }
