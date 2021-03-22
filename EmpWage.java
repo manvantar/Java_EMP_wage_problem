50067dbac32f3d785fc618c7a4ca2da8a34d3047
manvantar <manukvshetty@gmail.com>
2021-03-22 23:00:19 +0530

    Sloving using switchstatement

--- EmpWage.java
+++ EmpWage.java
@@ -1,26 +1,30 @@
+import java.util.*;
 class EmpWage{
 
 public static void main(String[] args){
 	int IS_FULL_TIME=1;
 	int IS_part_time=2;
 	int wage_per_hour=20;
-//	double empcheck=Math.floor(Math.random()*10)%2;
-double empcheck=Math.floor(Math.random()*10)%3;
+	int hours=0;
+	int wage_per_day=0;
+Random rand=new Random();
+	int empcheck=rand.nextInt(10)%3;
 System.out.println(empcheck+"\n");
-	if(empcheck==IS_FULL_TIME){
+	switch (empcheck){
+		case 1:
 		System.out.println("Employee is Full-timer");
-	int hours=12;
-	int wage_per_day=hours*wage_per_hour;
-	System.out.println(" wage_per_day ="+ wage_per_day);
-	}
-	else if(empcheck==IS_part_time){
-	 System.out.println("Employee is Part-timer");
-        int hours=8;
-        int wage_per_day=hours*wage_per_hour;
-        System.out.println(" wage_per_day ="+ wage_per_day);
-
-	}	
-	else{
+		hours=12;
+		wage_per_day=hours*wage_per_hour;
+		System.out.println(" wage_per_day ="+ wage_per_day);
+		break;
+		case 2:
+		System.out.println("Employee is Part-timer");
+        	hours=8;
+	        wage_per_day=hours*wage_per_hour;
+	        System.out.println(" wage_per_day ="+ wage_per_day);
+		break;
+	
+		default:
 		System.out.println("Employee is Absent");
 
 	}
