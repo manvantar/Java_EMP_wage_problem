7f176dd0092babe12023c64ebdf91f33b56413ab
manvantar <manukvshetty@gmail.com>
2021-03-22 22:40:13 +0530

    check whether employee is present or absent

--- EmpWage.java
+++ EmpWage.java
@@ -8,9 +8,9 @@ double empcheck=Math.floor(Math.random()*10)%2;
 System.out.println(empcheck+"\n");
 	if(empcheck==IS_FULL_TIME){
 		System.out.println("Employee is Present");
-		int hours=8;
-		int wage_per_day=hours*wage_per_hour;
-		System.out.println(" wage_per_day ="+ wage_per_day);
+	//	int hours=8;
+	//	int wage_per_day=hours*wage_per_hour;
+	//	System.out.println(" wage_per_day ="+ wage_per_day);
 	}	
 	else{
 		System.out.println("Employee is Absent");
